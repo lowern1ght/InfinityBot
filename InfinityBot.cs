7b1e2c876306bb4e89dfb021c32ce4771271d04f
Alexander Sergeev <bradwoord@gmail.com>
2023-04-09 17:11:47 +0500

    add icon, async main

--- InfinityBot.cs
+++ InfinityBot.cs
@@ -1,7 +1,14 @@
-﻿namespace InfinityBot;
+﻿using Humanizer;
+
+namespace InfinityBot;
 
 public class InfinityBot {
     public static async Task Main() {
         Visual.WriteHeader();
+        await new InfinityBot().StartBotAsync();
+    }
+
+    private async Task StartBotAsync() {
+        await Task.Delay(-1);
     }
 }
