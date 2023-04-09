278338bbe4d0648765c885e19c7f8a0837474d29
Alexander Sergeev <bradwoord@gmail.com>
2023-04-09 16:23:44 +0500

    edited header

--- Modules/Visual.cs
+++ Modules/Visual.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Pastel;
+using InfinityBot.Resources;
+using System.Runtime.InteropServices;
 
 namespace InfinityBot;
 
 internal static class Visual {
     public static void WriteHeader() {
+        Console.WriteLine(VisualAdditional.TitleText.Pastel(System.Drawing.Color.DarkSlateBlue)
+            + Environment.NewLine);
 
+        Console.WriteLine($" Author: {VisualAdditional.Author.Pastel(System.Drawing.Color.Gray)} "
+            +"<3".Pastel(ConsoleColor.Red));
     }
 }
