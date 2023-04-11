8cfb78061f6442d1d8d8a79659a7939d15e97f3c
Alexander Sergeev <bradwoord@gmail.com>
2023-04-11 23:46:46 +0500

    add iconfigure

--- InfinityBot.cs
+++ InfinityBot.cs
@@ -1,6 +1,15 @@
-﻿namespace InfinityBot;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace InfinityBot;
 
 public class InfinityBot {
+    private IConfiguration? Configuration { get; init; }
+
+    public InfinityBot() { }
+    public InfinityBot(IConfiguration configuration) {
+        Configuration = configuration;
+    }
+
     public static async Task Main() {
         Visual.WriteHeader();
         await new InfinityBot().StartBotAsync();
